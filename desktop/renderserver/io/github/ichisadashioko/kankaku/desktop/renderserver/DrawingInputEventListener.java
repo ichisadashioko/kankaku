152b6caaaf6af6610f28a865fc7476e6362e57a0
shioko <ichisadashioko@gmail.com>
2021-10-01 03:51:06 +0000

    remove stashed files

--- desktop/renderserver/io/github/ichisadashioko/kankaku/desktop/renderserver/DrawingInputEventListener.java
+++
@@ -1,9 +0,0 @@
-package io.github.ichisadashioko.kankaku.desktop.renderserver;
-
-public interface DrawingInputEventListener {
-    void onPenDown(int x, int y);
-
-    void onPenMove(int x, int y);
-
-    void onPenUp(int x, int y);
-}
